5ab2fd53dbefac380fc518b4897ffcd9171ec53e
Jon Catanio <joncatanio@gmail.com>
2016-10-28 06:58:04 -0500

    Push changes to aws

--- content.py
+++ content.py
@@ -18,3 +18,63 @@ def getcontent(year, month, day):
       return json.dumps({'message': 'no information for given date'}), 500
 
    return json.dumps(json.load(f)), 200
+
+@content_api.route("/getgames/<int:year>/<int:month>/<int:day>")
+def getgames(year, month, day):
+   curDate = date(year, month, day)
+
+   if year == None or month == None or day == None:
+      temp =  date.today()
+      curDate = date(temp.year, temp.month, temp.day)
+
+   try:
+      f = open('./datastore/' + curDate.isoformat() + '.txt', 'r')
+   except IOError:
+      return json.dumps({'message': 'no information for given date'}), 500
+
+   data = json.load(f)
+   games = []
+
+   for game in data:
+      obj = {}
+
+      obj['taglines'] = game['taglines']
+      obj['home'] = game['home']
+      obj['away'] = game['away']
+      obj['home_win'] = game['home_win']
+      obj['home_loss'] = game['home_loss']
+      obj['away_win'] = game['away_win']
+      obj['away_loss'] = game['away_loss']
+      obj['est_time'] = game['est_time']
+
+      rank = 0
+      for factor in game['rank_factors']['home']:
+         rank += int(factor['rank'])
+      for factor in game['rank_factors']['away']:
+         rank += int(factor['rank'])
+      obj['total_rank'] = str(rank)
+
+      games.append(obj)
+
+   return json.dumps(games), 200
+
+@content_api.route("/getgame/<int:year>/<int:month>/<int:day>/<string:home>/<string:away>")
+def getgame(year, month, day, home, away):
+   curDate = date(year, month, day)
+
+   if year == None or month == None or day == None:
+      temp =  date.today()
+      curDate = date(temp.year, temp.month, temp.day)
+
+   try:
+      f = open('./datastore/' + curDate.isoformat() + '.txt', 'r')
+   except IOError:
+      return json.dumps({'message': 'no information for given date'}), 500
+
+   data = json.load(f)
+
+   for game in data:
+      if game['home'] == home and game['away'] == away:
+         return json.dumps(game), 200
+
+   return json.dumps({'message': 'game not found'}), 404
